cf021cbf61113eca2db1185a52f2bab54e47eb08
Devin Jansa <devinjansa@Devins-MacBook-Pro-2.local>
2022-06-08 09:49:41 -0700

    index component finished, app component started

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from "react-router-dom";
+import Display from "./Components/Display";
+import Nav from "./Components/Nav";
 
 function App() {
   return (
     <div className="App">
-      <nav>
+      <main>
+        <Routes>
+          <Route path="" element={<Nav />} />
+          <Route path="" element={<Display />} />
 
-      </nav>
-      <Routes>
-        
-      </Routes>
+        </Routes>
+      </main>
     </div>
   );
 }
---
+++ src/Components/display.js
@@ -0,0 +1,4 @@
+import Controls from "./Components/Controls";
+
+//Route for Controls
+<Route path="" element={<Controls />} />
