5ca5e255f478cbc1172f209198d6045fcce62aa6
Trey Williams <kingproto3232@gmail.com>
2022-06-08 14:13:16 -0400

    Small fixes

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import { Routes, Route } from "react-router-dom";
 // import Display from "./Components/Display";
 import Nav from "./Components/Nav";
+import Display from './Components/Display';
+
 
 function App() {
   return (
     <div className="App">
       { <Nav />}
-
+      { <Display/>}
     </div>
   );
 }
--- src/Components/display.js
+++ src/Components/display.js
@@ -1,12 +1,10 @@
-import Controls from "./Components/Controls";
+// import controls from "./Components/Controls";
 import { useEffect, useState } from "react"
 import { Link } from "react-router-dom"
 import apiUrl from "../ApiUrl"
 
-//Route for Controls
-<Route path="" element={<Controls />} />
 
-const display = function() {
+const Display = function() {
 const [todo, setTodo] = useState({taskName: "", taskDetails: ""})
 
 
@@ -15,8 +13,7 @@ const [todo, setTodo] = useState({taskName: "", taskDetails: ""})
 return (
     <div>
         <form onSubmit={null}>
-            <input type="text" onChange={null} />
-            <input type="text" onChange={null} />
+            
             <input/>
         </form>
     </div>
