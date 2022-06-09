a740f2baffede4a5fd13bcae6778bce7c36bcc7e
Devin Jansa <devinjansa@Devins-MacBook-Pro-2.local>
2022-06-09 11:54:18 -0700

    began building controls for CRUD functionality

--- src/Components/Controls.js
+++ src/Components/Controls.js
@@ -1,21 +1,40 @@
 import React from 'react'
 import { useState } from 'react'
+import apiUrl from '../apiUrl'
 
 export default function Controls() {
 
-const [addTask, setAddTask] = useState("")
+const [todo, setTodo] = useState({taskName: "", taskDetails: ""})
+const [taskList, setTaskList] = useState()
+
 
 const handleChange = (event) => {
-    setAddTask(event.target.value)
+    setTodo({...taskList, [event.target.name]: event.target.value})
 }
-const handleSubmit = () => {
 
+const handleSubmit = (event) => {
+    event.preventDefault()
+    fetch(apiUrl + `/todos`, {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(todo)
+    })
+    .then(response => response.json())
+    .then(data => {
+        const copy = [...taskList]
+        copy.push(data.todo)
+        setTaskList({taskName: "", taskDetails: ""})
+    })
 }
 
   return (
     <div>
         <form onSubmit={handleSubmit}>
-            <input placeholder="Add Task" type="text" onChange={handleChange}/>
+            <input value={todo.taskName} placeholder="Add Task" type="text" onChange={handleChange}/>
+            <input value={todo.taskDetails} placeholder="Add Details" type="text" onChange={handleChange}/>
             <input type="submit" value="Add a Task" />
         </form>
     </div>
