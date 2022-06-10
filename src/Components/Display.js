f9c2c394888f4251ab7265fcebf37592c818c9e2
Nicholas Meade <nicholasjmeade@hotmail.com>
2022-06-09 20:14:39 -0400

    created a functioning delete button feature, deletes a task but does not update until page refresh

--- src/Components/Display.js
+++ src/Components/Display.js
@@ -2,20 +2,25 @@
 import { useEffect, useState } from "react"
 import { Link } from "react-router-dom"
 import apiUrl from "../apiUrl"
+import { useParams, useNavigate } from "react-router"
 
-const Display = function() {
+const Display = function () {
 // const [todo, setTodo] = useState({taskName: "", taskDetails: ""})
+// const [taskDisplay, setTaskDisplay] = useState("")
 const [interval, setInterval] = useState([])
 const [displayTask, setDisplayTask] = useState(false)
-// const [taskDisplay, setTaskDisplay] = useState("")
 
+const params = useParams()
+const navigate = useNavigate();
+
+// API call to fetch times data which contains tasks for certain times of the day
 useEffect(() => {
     fetch(apiUrl + `/times`)
         .then(response => response.json())
         .then(data => setInterval(data.times))
         .catch(() => console.log("broken"))
 }, [])
-// console.log(interval)
+
 const fries = () => {
     setDisplayTask(true)
 }
@@ -23,20 +28,41 @@ const toggleDisplay = () => {
     setDisplayTask(false)
 }
 
+// attempt of making a delete function work
+// can delete the task but does not update until refreshing the page
+// delete button ends up covering the task details when task name is expanded
+let taskDisplay = ""
 
 const mappedDisplay = interval.map((item, index) => {
-    // console.log(item._id)
-const tasks = item.tasks
-let taskDisplay = ""
-if (displayTask !== false) {
-    taskDisplay = (
-        tasks.map(task => <li onClick={toggleDisplay}>{task.taskDetails}</li>)
-    )
-}
+    const tasks = item.tasks
+    const mappedTasks = tasks.map((task, index) => {
+        const taskDelete = (event) => {
+            fetch(apiUrl + `/todos/` + task._id, {
+                method: "DELETE",
+            })
+            .then(() => navigate('/'))
+        }
+        if (displayTask !== false) {
+            taskDisplay = (
+                <div>
+                    <li onClick={toggleDisplay}>{task.taskDetails}<button onClick={taskDelete}>Delete</button></li>
+                </div>
+        )}
+    })
+
+// older mappedDisplay before trying to integrate a delete button/feature
+// const mappedDisplay = interval.map((item, index) => {
+// console.log(item._id)
+// const tasks = item.tasks
+// let taskDisplay = ""
+// if (displayTask !== false) {
+//     taskDisplay = (
+//         tasks.map(task => <li onClick={toggleDisplay}>{task.taskDetails}</li>)
+//     )
+// }
 
     return (
         <div key={index}>
-
             <h2 >
                 {item.timeOfDay}
             </h2>
@@ -51,10 +77,8 @@ if (displayTask !== false) {
 return (
     <div>
         {mappedDisplay}
-
     </div>
-        )
+    )
 }
 
 export default Display;
-//{/* <Route path="" element={<Controls />} /> */}
